f2155da878b021aa556bb973a91cbb65c77b6d49
EmirhanHasirciKodland <job.emirhanhasirci@gmail.com>
2025-06-01 21:12:14 +0300

    Update m1l2.py

--- m1l2.py
+++ m1l2.py
@@ -1,6 +1,6 @@
 import random
 karakterler = "+-/*!&$#?=@<>abcdefghijklnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890"
-sifre_uzunlugu = int( input("Şifrenin uzunluğunu seçiniz!"))
+sifre_uzunlugu = int( input("Şifrenin uzunluğunu ver"))
 sifre = ""
 for i in range(sifre_uzunlugu):
     sifre +=random.choice(karakterler)
